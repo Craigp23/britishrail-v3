43355408cb56bc08ec1b210e6c4b27bd0eb809d9
Craigp23 <77210330+Craigp23@users.noreply.github.com>
2026-05-29 21:28:46 +0100

    Update issue fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/britishrail-v3/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
